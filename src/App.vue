da9ef564cb0e5270b2385870ad82008eac37751c
Jochen Klar <mail@jochenklar.de>
2021-03-08 20:06:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Navbar :maps="maps" :currentView="currentView" :currentMap="currentMap" @changeMap="changeMap" />
+  <Map :currentView="currentView" :currentMap="currentMap" />
+</template>
+
+<script>
+  import Map from './components/Map.vue'
+  import Navbar from './components/Navbar.vue'
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        maps: this.$config.maps,
+        currentView: this.$config.view,
+        currentMap: this.$config.maps[0],
+      }
+    },
+    components: {
+      Map,
+      Navbar
+    },
+    methods: {
+      changeMap(index) {
+        this.currentMap = this.maps[index]
+      }
+    }
+  }
+</script>
---
+++ src/components/Map.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+  import "leaflet/dist/leaflet.css";
+  import L from "leaflet";
+
+  export default {
+    name: 'Map',
+    props: {
+      currentView: Object,
+      currentMap: Object
+    },
+    data() {
+      return {
+        map: null
+      }
+    },
+    methods: {
+      setupLeafletMap: function () {
+        if (this.map === null) {
+          this.map = L.map("map")
+        }
+
+        this.map.setView(this.currentView.center, this.currentView.zoom)
+        L.tileLayer(this.currentMap.url, this.currentMap.options).addTo(this.map)
+      }
+    },
+    watch: {
+      currentMap() {
+        this.setupLeafletMap()
+      }
+    },
+    mounted() {
+      this.setupLeafletMap()
+    }
+  }
+</script>
+
+<style scoped>
+  #map {
+    position: absolute;
+    width: 100%;
+    top: 55px;
+    bottom: 0;
+  }
+</style>
